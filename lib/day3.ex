c5fc77f6d7054c6104ebb2519109c935eb7e185b
adamdawkins <adam@dragondrop.uk>
2024-12-04 21:50:40 +0000

    03.1

--- lib/day3.ex
+++ lib/day3.ex
@@ -1,11 +1,14 @@
 defmodule Day3 do
   def load_input(filename) do
     File.read!(filename)
-    |> String.split("\n", trim: true)
   end
 
-  def part1(_input) do
-    "Part 1"
+  def part1(input) do
+    regex = ~r/mul\((\d{1,3}),(\d{1,3})\)/
+
+    Regex.scan(regex, input)
+    |> Enum.map(fn [_, x, y] -> String.to_integer(x) * String.to_integer(y) end)
+    |> Enum.sum()
   end
 
   def part2(_input) do
