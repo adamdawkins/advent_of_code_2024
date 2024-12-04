6b1815988ebef5beacc478dbb4a11a480db0cd2b
adamdawkins <adam@dragondrop.uk>
2024-12-04 22:18:27 +0000

    03.2: Refactor

--- lib/day3.ex
+++ lib/day3.ex
@@ -7,15 +7,15 @@ defmodule Day3 do
     regex = ~r/mul\((\d{1,3}),(\d{1,3})\)/
 
     Regex.scan(regex, input)
-    |> Enum.map(fn [_, x, y] -> String.to_integer(x) * String.to_integer(y) end)
-    |> Enum.sum()
+    |> extract_numbers()
+    |> multiply_and_sum()
   end
 
   def part2(input) do
     input
     |> process_commands()
-    |> Enum.map(fn {x, y} -> x * y end)
-    |> Enum.sum()
+    |> extract_numbers()
+    |> multiply_and_sum()
   end
 
   defp process_commands(input) do
@@ -26,7 +26,6 @@ defmodule Day3 do
     sections
     |> process_sections(true)
     |> Enum.flat_map(&Regex.scan(regex, &1))
-    |> Enum.map(fn [_full, num1, num2] -> {String.to_integer(num1), String.to_integer(num2)} end)
   end
 
   # don't do anything when the section is an empty list
@@ -45,5 +44,17 @@ defmodule Day3 do
   # When the next token (content) is anything else, carry on
   defp process_sections([content | rest], enabled) do
     [content | process_sections(rest, enabled)]
+  end
+
+  defp extract_numbers(regex_result) do
+    Enum.map(regex_result, fn [_full, num1, num2] ->
+      {String.to_integer(num1), String.to_integer(num2)}
+    end)
+  end
+
+  defp multiply_and_sum(numbers) do
+    numbers
+    |> Enum.map(fn {x, y} -> x * y end)
+    |> Enum.sum()
   end
 end
