f3d3f648f98728497f039b6896381b5724c5de8a
adamdawkins <adam@dragondrop.uk>
2024-12-12 14:20:20 +0000

    05.1: Passing

--- lib/day5.ex
+++ lib/day5.ex
@@ -1,11 +1,28 @@
 defmodule Day5 do
   def load_input(filename) do
-    File.read!(filename)
-    |> String.split("\n", trim: true)
+    lines =
+      File.read!(filename)
+      |> String.split("\n", trim: true)
+
+    Enum.reduce(lines, {[], []}, fn line, {ordering_rules, updates} ->
+      cond do
+        String.contains?(line, "|") ->
+          {[to_rule(line) | ordering_rules], updates}
+
+        String.contains?(line, ",") ->
+          {ordering_rules, [to_page(line) | updates]}
+
+        true ->
+          {ordering_rules, updates}
+      end
+    end)
   end
 
-  def part1(_input) do
-    "Part 1"
+  def part1({ordering_rules, updates}) do
+    updates
+    |> Enum.filter(&correct_order?(ordering_rules, &1))
+    |> Enum.map(&middle_page_number(&1))
+    |> Enum.sum()
   end
 
   def part2(_input) do
@@ -20,5 +37,28 @@ defmodule Day5 do
         {a, b} -> a < b
       end
     end)
+  end
+
+  defp middle_page_number(update) do
+    case length(update) do
+      # Return nil for empty array
+      0 -> nil
+      # Get the middle element
+      len -> Enum.at(update, div(len, 2))
+    end
+  end
+
+  defp to_page(line) do
+    line
+    |> String.split(",")
+    |> Enum.map(&String.to_integer/1)
+  end
+
+  defp to_rule(line) do
+    line
+    |> String.split("|")
+    |> Enum.map(&String.trim/1)
+    |> Enum.map(&String.to_integer/1)
+    |> List.to_tuple()
   end
 end
--- test/day5_test.exs
+++ test/day5_test.exs
@@ -127,4 +127,41 @@ defmodule Day5Test do
   # The last update, 97,13,75,29,47, is not in the correct order due to
   # breaking several rules.
   #
+  # Return the sum of the middle numbers of the updates in the correct order:
+  test "part 1" do
+    ordering_rules = [
+      {47, 53},
+      {97, 13},
+      {97, 61},
+      {97, 47},
+      {75, 29},
+      {61, 13},
+      {75, 53},
+      {29, 13},
+      {97, 29},
+      {53, 29},
+      {61, 53},
+      {97, 53},
+      {61, 29},
+      {47, 13},
+      {75, 47},
+      {97, 75},
+      {47, 61},
+      {75, 61},
+      {47, 29},
+      {75, 13},
+      {53, 13}
+    ]
+
+    updates = [
+      [75, 47, 61, 53, 29],
+      [97, 61, 53, 29, 13],
+      [75, 29, 13],
+      [75, 97, 47, 61, 53],
+      [61, 13, 29],
+      [97, 13, 75, 29, 47]
+    ]
+
+    assert Day5.part1({ordering_rules, updates}) == 143
+  end
 end
