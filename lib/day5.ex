a3bf7c3681a6be98f1e827a7f576885a1f2234e6
adamdawkins <adam@dragondrop.uk>
2024-12-12 14:11:51 +0000

    05.1: Add correct_order?

--- lib/day5.ex
+++ lib/day5.ex
@@ -11,4 +11,14 @@ defmodule Day5 do
   def part2(_input) do
     "Part 2"
   end
+
+  def correct_order?(ordering_rules, update) do
+    Enum.all?(ordering_rules, fn {a, b} ->
+      case {Enum.find_index(update, &(&1 == a)), Enum.find_index(update, &(&1 == b))} do
+        {nil, _} -> true
+        {_, nil} -> true
+        {a, b} -> a < b
+      end
+    end)
+  end
 end
